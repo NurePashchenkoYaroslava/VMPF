c9cbbe65ccf7743544b32dec6f5aa3fcc6f5991b
ноут-коут <-@DESKTOP-11N7BAB>
2026-03-14 08:08:22 +0200

    first practic

---
+++ Practic1.py
@@ -0,0 +1,19 @@
+# name = input("Введіть ваше ім'я: ")
+# print (f"Привітик, {name}")
+
+# numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# for number in numbers:
+#     print(number, number**2)
+
+# numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# def parnic(numbers):
+#     result = []
+#     for number in numbers:
+#         if isinstance(number, int) and number % 2 == 0:
+#             result.append(number)
+#     return(result)
+# print(parnic(numbers))
+
+# import requests
+# apigetter = requests.get('https://catfact.ninja/fact')
+# print(apigetter.json())
